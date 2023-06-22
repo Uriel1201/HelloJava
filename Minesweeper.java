22b223f242c20b6ea9748536356ae1c822a043e4
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-06-22 14:11:02 -0600

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -5,26 +5,33 @@
 
 
 public class Minesweeper {
+    
     public static void main(String[] args) {
+        
         int m = Integer.parseInt(args[0]);
         int n = Integer.parseInt(args[1]);
         int k = Integer.parseInt(args[2]);
         int t = m * n;
+        
         boolean[] shuffle = new boolean[t];
+       
         for (int h = 0; h < k; h++)
             shuffle[h] = true;
+        
         for (int h = 0; h < t; h++) {
             int p = h + (int) (Math.random() * (t - h));
             boolean s = shuffle[p];
             shuffle[p] = shuffle[h];
             shuffle[h] = s;
         }
+        
         boolean[][] mines = new boolean[m + 2][n + 2];
         for (int h = 1; h < m + 1; h++) {
             for (int i = 1; i < n + 1; i++) {
                 mines[h][i] = shuffle[n * (h - 1) + i - 1];
             }
         }
+        
         int[][] count = new int[m + 2][n + 2];
         int row = 1;
         while (row < m + 1) {
@@ -37,6 +44,7 @@ public class Minesweeper {
             }
             row++;
         }
+        
         for (int i = 1; i < m + 1; i++) {
             for (int j = 1; j < n + 1; j++) {
                 if (mines[i][j]) System.out.print("*  ");
