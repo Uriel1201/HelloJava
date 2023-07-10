eb7121570d87fd22011de5b14177d19d6bbc3025
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-07-10 16:50:54 -0600

    Update GeneralizedHarmonic.java

--- GeneralizedHarmonic.java
+++ GeneralizedHarmonic.java
@@ -1,1 +1,28 @@
+/* *****************************************************************************
+ *  Name:              Dante1201 (Carlos Uriel García)
+ *  Id:                1201
+ **************************************************************************** */
 
+public class GeneralizedHarmonic {
+
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(args[0]);
+        int r = Integer.parseInt(args[1]);
+
+        if (n > 0) {
+            double s = 1.0 * r;
+            double sum = 0.0;
+            for (int i = 1; i <= n; i++) {
+                double j = 1.0 * i;
+                double term = 1.0 / Math.pow(j, s);
+                sum += term;
+            }
+            System.out.println(sum);
+
+        }
+        else {
+            System.out.println("n must to be positive.");
+        }
+    }
+}
