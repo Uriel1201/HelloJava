a003f7528d5a3c6a4fac5e6a524369f1aeb250f3
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-10-21 23:43:40 -0600

    Update BinomialDist.java

--- BinomialDist.java
+++ BinomialDist.java
@@ -1,14 +1,17 @@
 public class BinomialDist {
 
-  int n = Integer.parseInt(args[0]);
-  double[][] weights = new double[n + 1][n + 1]; 
-  weights[1][1] = 1.0;
+  public static void main(String[] args) {
+    
+    int n = Integer.parseInt(args[0]);
+    double[][] weights = new double[n + 1][n + 1]; 
+    weights[1][1] = 1.0;
 
-  for (int i = 2; i < n + 1; i++) {
-    for (int j = 1; j < i + 1; j++) {
-      weights[i][j] = (weights[i - 1][j - 1] + weights[i - 1][j]) * 0.5;
-      System.out.print(weights[][] + " *");
+    for (int i = 2; i < n + 1; i++) {
+      for (int j = 1; j < i + 1; j++) {
+        weights[i][j] = (weights[i - 1][j - 1] + weights[i - 1][j]) * 0.5;
+        System.out.print(weights[][] + " *");
+      }
+      System.out.println();
     }
-    System.out.println();
   }
 }
