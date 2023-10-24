51132777097b48aef11163edf60fc39ec87c5314
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-10-24 15:10:39 -0600

    Update BinomialDist.java

--- BinomialDist.java
+++ BinomialDist.java
@@ -3,16 +3,22 @@ public class BinomialDist {
   public static void main(String[] args) {
     
     int n = Integer.parseInt(args[0]);
-    double[][] weights = new double[n + 1][n + 1]; 
-    weights[1][1] = 1.0;
-    System.out.println(weights[1][1]);
+    int x = Integer.parseInt(args[1]);
+    double p = Double.parseDouble(args[1]);
+    int[][] triangle = new int[n + 1][n + 2]; 
+    triangle[0][1] = 1;
 
-    for (int i = 2; i < n + 1; i++) {
-      for (int j = 1; j < i + 1; j++) {
-        weights[i][j] = (weights[i - 1][j - 1] + weights[i - 1][j]) * 0.5;
-        System.out.print(weights[i][j] + " ");
+    for (int i = 1; i < n + 1; i++) {
+      for (int j = 1; j < i + 2; j++) {
+        triangle[i][j] = (triangle[i - 1][j - 1] + triangle[i - 1][j]);
       }
-      System.out.println();
     }
+
+    double w = Math.pow(p, x);
+    double q = 1 - p;
+    int m = n - x;
+    double v = Math.pow(q, m);
+    double weight = w * v;
+    System.out.println("P(X = x): " + (weight * triangle[n][x + 1]);
   }
 }
