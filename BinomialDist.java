ca0f41d2a7858ddf1598d02bf7ddbe02b5ad662d
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-10-26 16:54:35 -0600

    Update BinomialDist.java

--- BinomialDist.java
+++ BinomialDist.java
@@ -59,13 +59,54 @@ public class BinomialDist {
   } 
 
   /************************************************/
+  public static double getProbability(int n, int x, double p) {
+    
+    // x: 'number of endpoints' 0 <= x <= n
+    // n: 'number of trials'    0 < n
+    // p: 'success probability' 0 < p < 1
+    
+    BigInteger[][] triangle = pascal(n);
+    
+    int i = 0;
+    double _p = 1;
+    while (i < x) {
+      _p = _p * p;
+      i++;
+    }
+
+    int y = n - x;
+    int j = 0;
+    double q = 1 - p;
+    double _q = 1;
+    while (j < y) {
+      _q  = _q * q;
+      j++;
+    }
+    
+    double comb = triangle[n][x + 1].doubleValue();
+    double w = _p * _q * comb;
+    return w;
+  }
+
+  /************************************************/
   public static void main(String[] args) {
     
-    int n = Integer.parseInt(args[0]); 
-    double p = Double.parseDouble(args[1]);
-    double[] w = weights(n, p);
+    int n = Integer.parseInt(args[0]);
+    int x =  Integer.parseInt(args[1]);
+    double p = Double.parseDouble(args[2]);
     
+    
+    double[] w = weights(n, p);
+    double sProb1 = getProbability(n, x, p);
+    int y = n - x;
+    double sProb2 = getProbability(n, y, p);
+    Boolean match1 = (sProb1 == w[x]);
+    Boolean match2 = (sProb2 == w[y]);
+    /*
     for (int i = 0; i < n + 1; i++)
       System.out.println(w[i]);
+    */
+    System.out.println(match1);
+    System.out.println(match2);
   }
 }
