e9a8d4f9ccad3201607328a29e8cded5cb999020
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-11-24 20:01:13 -0600

    Update PolynomialRoots.java

--- PolynomialRoots.java
+++ PolynomialRoots.java
@@ -1,70 +1,81 @@
-import org.ejml.data.Complex_F64;
 import org.ejml.data.DMatrixRMaj;
-import org.ejml.dense.row.factory.DecompositionFactory_DDRM;
 import org.ejml.dense.row.CommonOps_DDRM;
-import org.ejml.interfaces.decomposition.QRDecomposition_F64;
-  
+import org.ejml.dense.row.factory.DecompositionFactory_DDRM;
+import org.ejml.interfaces.decomposition.QRDecomposition;
+
+
 public class PolynomialRoots {
 
-  /*******************************************************************/
-  private static DMatrixRMaj getCompanionM(double[] coefficients) {
+    /*******************************************************************/
+    private static DMatrixRMaj getCompanionM(double[] coefficients) {
 
-    int n = coefficients.length - 1;
-    DMatrixRMaj comp = new DMatrixRMaj(n, n);
-    double monic = coefficients[n];
-    
-    for (int i = 0; i < n; i++) {
-      comp.set(i, n - 1, -1.0 * coefficients[i] / monic);
-    }
-    for (int i = 0; i < n; i++) {
-      comp.set(i, i - 1, 1.0);
-    }
+        int n = coefficients.length - 1;
+        DMatrixRMaj comp = new DMatrixRMaj(n, n);
+        double monic = coefficients[n];
 
-    return comp;
-  }
-  
-  
-  /*******************************************************************/
-  private static boolean isUpperTriangular(DMatrixRMaj matrix, double error) {
-
-    for (int i = 0; i < matrix.getNumRows(); i++) {
-      for (int j = 0; j < i; j++) {
-        if (Math.abs(matrix.get(i, j)) > error) {
-          return false;
+        for (int i = 0; i < n; i++) {
+            comp.set(i, n - 1, -1.0 * coefficients[i] / monic);
         }
-      }
-    }
-    return true;
-  }
+        for (int i = 0; i < n; i++) {
+            comp.set(i, i - 1, 1.0);
+        }
 
-  
-  /*******************************************************************/
-  public static Complex_F64[] getRoots(double[] coefficients) {
-
-    DMatrixRMaj companion = getCompanionM(coefficients);
-    int n = coefficients.length - 1;
-
-    int maxIterations = 10000;
-    double eps = 1.0E-10;
-
-    for (int i = 0; i < maxIterations; i++) {
-      
-      QRDecomposition_F64<DMatrixRMaj> qr = DecompositionFactory_DDRM.qrp(n, n);
-      qr.decompose(companion);
-      DMatrixRMaj q = qr.getQ(null, false);
-      DMatrixRMaj r = qr.getR(null, false);
-      companion = CommonOps_DDRM.mult(r, q, companion);
-
-      if (isUpperTriangular(companion, eps)) {
-        break;
-      }
+        return comp;
     }
 
-    Complex_F64[] roots = new Complex_F64[n];
-    for (int i = 0; i < n; i++) {
-      roots[i] = companion.get(i, i);
+
+    /*******************************************************************/
+    private static boolean isUpperTriangular(DMatrixRMaj matrix, double error) {
+
+        for (int i = 0; i < matrix.getNumRows(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (Math.abs(matrix.get(i, j)) > error) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
-    
-    return roots;
-  }
+
+
+    /*******************************************************************/
+    public static double[] getRoots(double[] coefficients) {
+
+        DMatrixRMaj companion = getCompanionM(coefficients);
+        int n = coefficients.length - 1;
+
+        int maxIterations = 10000;
+        double eps = 1.0E-10;
+
+        for (int i = 0; i < maxIterations; i++) {
+
+            QRDecomposition<DMatrixRMaj> qr = DecompositionFactory_DDRM.qr(n, n);
+            qr.decompose(companion);
+            DMatrixRMaj q = qr.getQ(null, false);
+            DMatrixRMaj r = qr.getR(null, false);
+            companion = CommonOps_DDRM.mult(r, q, companion);
+
+            if (isUpperTriangular(companion, eps)) {
+                break;
+            }
+        }
+
+        double[] roots = new double[n];
+        for (int i = 0; i < n; i++) {
+            roots[i] = companion.get(i, i);
+        }
+
+        return roots;
+    }
+
+    public static void main(String[] args) {
+        double[] coef = new double[4];
+        coef[0] = 1.0;
+        coef[1] = -5.0;
+        coef[2] = 0.0;
+        coef[3] = 1.0;
+        double[] r = getRoots(coef);
+        for (int i = 0; i < coef.length; i++)
+            System.out.println(r[i]);
+    }
 }
