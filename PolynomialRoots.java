832eb78eba558252da17495d59fd05755ee5c43f
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2023-11-25 00:02:21 -0500

    Update PolynomialRoots.java

--- PolynomialRoots.java
+++ PolynomialRoots.java
@@ -7,6 +7,7 @@ import org.ejml.interfaces.decomposition.QRDecomposition;
 public class PolynomialRoots {
 
     /*******************************************************************/
+    // Returns the Companion Matrix of a polynomial 
     private static DMatrixRMaj getCompanionM(double[] coefficients) {
 
         int n = coefficients.length - 1;
