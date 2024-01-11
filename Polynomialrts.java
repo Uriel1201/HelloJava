18eb8b592b654d65de4472dabdf2629c2152c6ca
Uriel1201 <128760419+Uriel1201@users.noreply.github.com>
2024-01-11 17:28:49 -0500

    Update Polynomialrts.java

--- Polynomialrts.java
+++ Polynomialrts.java
@@ -4,6 +4,7 @@ import org.ejml.dense.row.factory.DecompositionFactory_DDRM;
 import org.ejml.dense.row.CommonOps_DDRM;
 import org.ejml.interfaces.decomposition.QRDecomposition;
 
+/*******************************************************************/
 
 public class Polynomialrts {
 
