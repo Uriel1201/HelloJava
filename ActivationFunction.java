3d5307a855355d0d1243d5a221c5adb85ec1be67
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-01-27 15:23:15 -0500

    Update ActivationFunction.java

--- ActivationFunction.java
+++ ActivationFunction.java
@@ -5,6 +5,7 @@
 
 public class ActivationFunction {
 
+    /************************************************/
     public static double heaviside(double x) {
         if (Double.isNaN(x)) return Double.NaN;
         else if (x < 0.0) return 0.0;
@@ -12,6 +13,8 @@ public class ActivationFunction {
         else return 1.0;
     }
 
+    
+    /************************************************/
     public static double sigmoid(double x) {
         if (Double.isNaN(x)) return Double.NaN;
         x = -x;
@@ -19,6 +22,8 @@ public class ActivationFunction {
         return s;
     }
 
+
+    /************************************************/
     public static double tanh(double x) {
         if (Double.isNaN(x)) return Double.NaN;
         else if (x >= 20.0) return 1.0;
@@ -31,6 +36,8 @@ public class ActivationFunction {
         }
     }
 
+    
+    /************************************************/
     public static double softsign(double x) {
         if (Double.isNaN(x)) return Double.NaN;
         else if (x == Double.POSITIVE_INFINITY) return 1.0;
@@ -38,6 +45,8 @@ public class ActivationFunction {
         else return x / (1.0 + Math.abs(x));
     }
 
+
+    /************************************************/
     public static double sqnl(double x) {
         if (Double.isNaN(x)) return Double.NaN;
         else if (x <= -2.0) return -1.0;
@@ -46,6 +55,8 @@ public class ActivationFunction {
         else return 1.0;
     }
 
+
+    /************************************************/
     public static void main(String[] args) {
         double act = Double.parseDouble(args[0]);
         System.out.println("heaviside(" + act + ") = " + heaviside(act));
