dd0fb3e5ee1b94b30980856df764b4951f6f8149
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-01-31 11:46:18 -0500

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -61,17 +61,22 @@ public class Divisors {
         if (n < 0) {
             throw new IllegalArgumentException("n must be positive");
         }
+        
         int phi = n;
         for (int i = 2; i * i <= n; i++) {
+            
             if (n % i == 0) {
+                
                 while (n % i == 0) {
                     n /= i;
                 }
+                
                 phi -= phi/i;
             }
-            if (n > 1) {
-                phi -= phi / n;
-            }
+        }
+
+        if (n > 1) {
+            phi -= phi/n;
         }
 
         return phi;
