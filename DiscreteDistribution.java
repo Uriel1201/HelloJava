dc2390ab1a0344d153c6d28a19dcdb09cadf0016
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-02-02 18:05:14 -0500

    Update DiscreteDistribution.java

--- DiscreteDistribution.java
+++ DiscreteDistribution.java
@@ -4,7 +4,7 @@
  *
  *  # DISCRETE DISTRIBUTION SIMULATION 
  *  **********************************
- *  This script presents a simple algorithm for sampling
+ *  This class presents a simple algorithm for sampling
  *  from a discrete probability distribution p(x) defined over 
  *  a countably finite set.
  *  In probability theory, this is known as sampling from a 
@@ -15,29 +15,42 @@
  *  *************************************************************************** */
 
 public class DiscreteDistribution {
-   
-    public static void main(String[] args) {
-        
-        int n = args.length;
-        int m = Integer.parseInt(args[0]);
-        
-        int[] a = new int[n - 1];
-        for (int i = 0; i < n - 1; i++) {
+
+   public static void main(String[] args) {
+
+      int n = args.length;
+      int m = Integer.parseInt(args[0]); // Size of the sample 
+
+      if (m < 0) {
+         throw new IllegalArgumentException("The sample size must be positive");
+      }
+      
+      int[] a = new int[n - 1];
+      for (int i = 0; i < n - 1; i++) {
+         
+         if (args[i + 1] < 0) {
+            throw new IllegalArgumentException("This value must represent a measure");
+         }
+         else {
             a[i] = Integer.parseInt(args[i + 1]);
-        }
-        
-        int[] s = new int[n];
-        for (int i = 1; i < n; i++) {
-            s[i] = s[i - 1] + a[i - 1];
-        }
-        
-        for (int k = 0; k < m; k++) {
-            int p = (int) (Math.random() * s[n - 1]);
-           
-            for (int j = 1; j < n; j++) {
-                if (s[j] > p && s[j - 1] <= p)
-                    System.out.print(j + "  ");
+         }
+      }
+
+      int[] s = new int[n];
+      for (int i = 1; i < n; i++) {
+         
+         s[i] = s[i - 1] + a[i - 1];
+      }
+
+      for (int k = 0; k < m; k++) {
+         
+         int p = (int) (Math.random() * s[n - 1]);
+         for (int j = 1; j < n; j++) {
+            
+            if (s[j] > p && s[j - 1] <= p) {
+               System.out.print(j + "  ");
             }
-        }
-    }
+         }
+      }
+   }
 }
