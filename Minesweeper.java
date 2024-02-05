7eda9f3916cda783b54de74d781ef7dd20b2da22
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-02-05 17:28:54 -0500

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,9 +1,27 @@
 /* *****************************************************************************
  *  Name:              Dante1201 (Carlos Uriel García)
  *  Id:                1201
- **************************************************************************** */
+ *
+ *  # THE MINESWEEPER GAME
+ *  **********************
+ *  Minesweeper is single-player logic-based computer game played 
+ *  on rectangular board whose object is to locate 
+ *  a predetermined number of randomly-placed "mines" 
+ *  in the shortest possible time by clicking on "safe" squares 
+ *  while avoiding the squares with mines.
+ *
+ *  The goal is to deduce which cells contain hidden mines using clues about 
+ *  the number of mines in neighboring cells.
+ *  *************************************************************************** */
 
 public class Minesweeper {
+
+    // > 1. Generate an m-by-n grid of cells, 
+    //      with exactly k of the mn cells containing mines, 
+    //      uniformly at random.
+    // > 2. For each cell not containing a mine, count the number of neighboring mines 
+    //      (above, below, left, right, or diagonal).
+    // > 3. Black for the cells containing a mine.
     
     public static void main(String[] args) {
         
@@ -11,14 +29,17 @@ public class Minesweeper {
         if (m <= 0) {
             throw new IllegalArgumentException("m must be positive");
         }
+        
         int n = Integer.parseInt(args[1]); // Number of Columns 
         if (n <= 0) {
             throw new IllegalArgumentException("n must be positive");
         }
+        
         int k = Integer.parseInt(args[2]); // Number of Mines 
         if (k <= 0) {
             throw new IllegalArgumentException("k must be positive");
         }
+        
         int t = m * n; // Maximum Capacity 
         if (k > t) {
             throw new IllegalArgumentException("the number of mines surpassed the max capacity");
