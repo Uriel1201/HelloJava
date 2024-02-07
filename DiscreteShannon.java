340926e040485df7aefe4ec501cc4e49d1c37f2e
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-02-07 17:29:08 -0500

    Update DiscreteShannon.java

--- DiscreteShannon.java
+++ DiscreteShannon.java
@@ -19,3 +19,77 @@
  *    It will be decreased unless this experience is 
  *    irrelevant (independent), in which case uncertainty is not changed
  *  *************************************************************************** */
+
+public class DiscreteShannon {
+
+  /****************************************************************************/
+  public static distBer(double p) {
+
+    if (p > 1 || p < 0) {
+      throw new IllegalArgumentException("p must represent a probability measure");
+    }
+    
+    double[] ber = new double[2];
+    ber[0] = 1.0 - p;
+    ber[1] = 1.0;
+
+    return ber;
+  }
+}
+
+def sampling(dist):
+    u = np.random.rand()
+    n = len(dist)
+    if u <= dist[0]:
+        return 0
+    for i in range(1, n):
+        if u > dist[i - 1] and u <= dist[i]:
+            return i
+    return n
+
+
+#----------------------------------------------------
+def discreteShannon(array, m):
+    if m < 0:
+        raise ValueError(f'm must be a cardinal')
+    f = np.zeros(m, dtype = int)
+    n = len(array)
+    for i in range(n):
+        if array[i] < 0:
+            raise ValueError(f'The entries must be no negative')
+        elif array[i] == 0:
+            f[0] += 1
+        elif array[i] < m:
+            f[array[i]] += 1
+    h = 0.0
+    for i in range(m):
+        if f[i] > 0:
+            p = 1.0 * f[i] / n
+            arg = -1.0 * p * np.log(p) / np.log(2)
+            h += arg
+    return h
+
+
+#----------------------------------------------------
+def main():
+    lz = 50
+    x = np.linspace(0, 1, lz, dtype = float)
+    y = []
+    for i in range(len(x)):
+        dist = distBer(x[i])
+        a = []
+        t = 1000
+        for j in range(t):
+            a.append(sampling(dist))
+        a_array = np.array(a)
+        y.append(discreteShannon(a_array, 2))
+    y_array = np.array(y)
+    plt.figure(figsize = (8, 9))
+    sns.set(style = 'darkgrid')
+    plt.title(f'{lz} SAMPLES OF SIZE {t}')
+    plt.ylabel(f'Estimated Uncertainty')
+    plt.xlabel(f'sample with ~ Ber(p)')
+    sns.scatterplot(x = x, y = y_array, color = '#088F8F')
+    plt.tight_layout()
+    plt.show()
+  
