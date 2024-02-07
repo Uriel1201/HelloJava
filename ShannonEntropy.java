8bacda027b2f6e2a5e8ff0a0bef910120b08881f
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-02-07 15:19:41 -0500

    Update ShannonEntropy.java

--- ShannonEntropy.java
+++ ShannonEntropy.java
@@ -1,7 +1,25 @@
-/* *****************************************************************************
+/*  *****************************************************************************
  *  Name:              Dante1201 (Carlos Uriel García)
  *  Id:                1201
- **************************************************************************** */
+ *  
+ *  # THE DISCRETE SHANNON ENTROPY
+ *  ******************************
+ *  The qualities of this formula "H" play a central role 
+ *  in information theory as measure of information, 
+ *  choice and uncertainty. 
+ *  This measure for example is the H in 
+ *  Boltzmann's famous H Theorem.
+ *
+ *  This quantity has some interesting properties:
+ *  > 1. When we are certain of the outcomes in a set of observations 
+ *    does H vanish, otherwise H is positive.
+ *  > 2. For a total of m possible results in a set of observations, 
+ *    H is maximum when all proportions of this possibilities are equal. 
+ *    This is also the most uncertain situation.
+ *  > 3. The uncertainty is never increased by experience. 
+ *    It will be decreased unless this experience is 
+ *    irrelevant (independent), in which case uncertainty is not changed
+ *  *************************************************************************** */
 
 public class ShannonEntropy {
   
