38cf93747d083f3d4a2823cdcb72adea79a891a3
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-02-14 16:58:57 -0500

    Update TrinomialBrute.java

--- TrinomialBrute.java
+++ TrinomialBrute.java
@@ -1,11 +1,22 @@
 /* *****************************************************************************
  *  Name:              Dante1201 (Carlos Uriel García)
  *  Id:                1201
- **************************************************************************** */
+ * 
+ *  #TRINOMIAL COEFFICIENTS
+ *  ***********************
+ *  Trinomial coefficients arise in combinatorics. The trinomial coefficient, 
+ *  denoted as T(n, k), represents the coefficient of x^(n+k) in the expansion 
+ *  of (1+x+x^2)^n. This class implements a recursive function in a brute-force manner 
+ *  to compute the coefficients by utilizing their corresponding recurrence relation. 
+ *  However, this approach may not be the most efficient way to solve this problem.
+ *  *************************************************************************** */
 
 public class TrinomialBrute {
 
     public static long trinomial(int n, int k) {
+
+        /**
+          **/
         if (n == 0 && k == 0) return 1;
         else if (k < -n || n < k) return 0;
         else return trinomial(n - 1, k - 1) + trinomial(n - 1, k) + trinomial(n - 1, k + 1);
