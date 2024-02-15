57133aa86c539ed07b741c772d3aab3eab2a9331
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2024-02-15 01:24:40 -0500

    Update TrinomialDP.java

--- TrinomialDP.java
+++ TrinomialDP.java
@@ -6,7 +6,7 @@
  *  ***********************
  *  Trinomial coefficients arise in combinatorics. The trinomial coefficient,
  *  denoted as T(n, k), represents the coefficient of x^(n+k) in the expansion
- *  of (1+x+x^2)^n. This script computes the trinomial coefficient T(n,k) 
+ *  of (1+x+x^2)^n. This class computes the trinomial coefficient T(n,k) 
  *  using dynamic programming. This version should be fast enough to handle 
  *  larger values of n and k.
  *  *************************************************************************** */
@@ -14,22 +14,44 @@
 public class TrinomialDP {
 
     public static long trinomial(int n, int k) {
+
+        /**
+          *  The trinomial coefficient for the integers n, k
+          *  @param n The exponent integer in the expansion of (1+x+x^2)^n
+          *  @param k The integer in the corresponding term x^n+k
+          *  @return the trinomial coefficient utilizing dynamic programming 
+          *  @throws IllegalArgumentException if n is negative 
+          */
+        if (n < 0) {
+
+            throw new IllegalArgumentException("n must be non-negative");
+        }
         
         if (k < 0) k = -k;
-        if (k > n) return 0;
+        
+        if (k > n) 
+            return 0;
         
         else {
+            
             long[][] triangle = new long[n + 1][2 * (n + 1)];
             triangle[0][1] = 1;
             
             for (int i = 1; i < n + 1; i++) {
+                
                 for (int j = 1; j < 2 * (i + 1); j++) {
-                    if (j == 1) triangle[i][j] = 1;
-                    else triangle[i][j] = triangle[i - 1][j]
+                    
+                    if (j == 1) 
+                        
+                        triangle[i][j] = 1;
+                    else 
+                        
+                        triangle[i][j] = triangle[i - 1][j]
                                         + triangle[i - 1][j - 1]
                                         + triangle[i - 1][j - 2];
                 }
             }
+            
             return triangle[n][n + k + 1];
         }
 
@@ -39,10 +61,6 @@ public class TrinomialDP {
         
         int n = Integer.parseInt(args[0]);
         int k = Integer.parseInt(args[1]);
-       
-        if (n < 0)
-            System.out.println("Error: n must to be no negative");
-        else
-            System.out.println(trinomial(n, k));
+        System.out.println(trinomial(n, k));
     }
 }
